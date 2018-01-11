a8770accc2e8536486b64d064556041ed3a26dd0
用心 <@172.16.143.7>
2018-01-11 15:27:14 +0800

    test

--- src/main/java/com/jlblog/jlblog/controller/ArticleController.java
+++ src/main/java/com/jlblog/jlblog/controller/ArticleController.java
@@ -6,14 +6,16 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.jlblog.jlblog.pojo.Article;
+import com.jlblog.jlblog.pojo.ArticleVo;
+import com.jlblog.jlblog.pojo.Laber;
 import com.jlblog.jlblog.service.ArticleService;
 
 @Controller
-@RequestMapping("/article")
 public class ArticleController {
 
 	@Autowired
@@ -21,31 +23,50 @@ public class ArticleController {
 	
 	/**
 	 * 根据分类id查询文章
-	 * 
 	 * @param id
 	 * @return
-	 * 
 	 */
-	@RequestMapping("/getActicleByClassify")
+	@RequestMapping("/article/getActicleByClassify")
 	@ResponseBody
-	public Map getActicleByClassify(String id){
-		int b = 123;
-		int e = 456;
-		int aa=121231;
-		String s = "hsdhf";
+	public Map getActicleByClassify(String classify){
+	
 		Map map = new HashMap();
-		if (id==null || id.equals("")) {
+		if (classify==null || classify.equals("")) {
 			map.put("error", 0);
+			map.put("msg", "参数为空，无法获取！");
 			return map;
 		}
-		List<Article> articleList = articleService.getActicleByClassify(id);
-		
+		List<ArticleVo> articleList = articleService.getActicleByClassify(classify);
+		if (articleList==null) {
+			map.put("error", 0);
+			map.put("msg", "查询数据失败！请重试！");
+			return map;
+		}
 		map.put("error", 200);
 		map.put("msg", articleList);
 		return map;
 	}
-	public void test(){
+	
+	@RequestMapping("/article/getArticleById")
+	@ResponseBody
+	public Map getArticleById(String id){
 		
+		Map map = new HashMap();
+		if (id==null || id.equals("")) {
+			map.put("error", 0);
+			map.put("msg", "参数为空，无法获取！");
+			return map;
+		}
+		List<ArticleVo> list = articleService.getArticleById(id);
+		if (list==null) {
+			map.put("error", 0);
+			map.put("msg", "查询数据失败！请重试！");
+			return map;
+		}
+		map.put("error", 200);
+		map.put("msg", list.get(0));
+		
+		return map;
 	}
 	
 }
